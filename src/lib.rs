39780dbe9ecf69fc17a81f058d8919e408d03edc
Jernej Kos <jernej@kos.mx>
2022-12-12 15:04:27 +0100

    Refactor and sort policy signers for Testnet

--- src/lib.rs
+++ src/lib.rs
@@ -18,19 +18,11 @@ pub fn trusted_policy_signers() -> TrustedPolicySigners {
     if is_testnet() {
         // Testnet.
         TrustedPolicySigners {
-            signers: {
-                let mut signers = HashSet::new();
-                signers.insert(
-                    "c37cbd0345965fda84fbaa372a01fc840b7b66eebfeb66dfdd35bb3e801f2cf3".into(),
-                );
-                signers.insert(
-                    "df8ca9fc78ce2c01f8217e8ce7aa582e8952545f412426fe07d42ca119e12166".into(),
-                );
-                signers.insert(
-                    "b27b3d0245d4cbd78be8e04e473f36abee350fcfbc438000313db1bb06117a43".into(),
-                );
-                signers
-            },
+            signers: HashSet::from([
+                "b27b3d0245d4cbd78be8e04e473f36abee350fcfbc438000313db1bb06117a43".into(),
+                "c37cbd0345965fda84fbaa372a01fc840b7b66eebfeb66dfdd35bb3e801f2cf3".into(),
+                "df8ca9fc78ce2c01f8217e8ce7aa582e8952545f412426fe07d42ca119e12166".into(),
+            ]),
             threshold: 2,
         }
     } else {
